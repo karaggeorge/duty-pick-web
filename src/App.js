ae1d2be219617802518a67c22505d4d164cafa11
George Karagkiaouris <karaggeorge0@gmail.com>
2017-09-19 17:15:46 -0400

    Remove react-snapshot

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Helmet from 'react-helmet'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet title="Duty Pick" />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
