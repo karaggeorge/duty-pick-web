5621098a388110f0f36e295996299b0e54b670c8
George Karagkiaouris <karaggeorge0@gmail.com>
2017-09-19 16:49:04 -0400

    Add API proxy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from 'react-snapshot'
+// import { render } from 'react-snapshot'
+import ReactDOM from 'react-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
